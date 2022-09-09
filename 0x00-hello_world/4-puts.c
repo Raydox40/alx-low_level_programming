bcf912cb3db9fda23a31a36a4a563e6ee535de03
Raydox40 <raydox40@gmail.com>
2022-09-08 19:53:20 -0700

    remaining task on c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - print result
+ *
+ *
+ *
+ * Return: Always 0 (Executed sucessful)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual
+			puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Printing texts with outcomr as art
+ *
+ *
+ *
+ * Return: Always 0 (Executed Success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of
+			art,\n");
+	return (0);
+}
